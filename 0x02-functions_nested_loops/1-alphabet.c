79320ff20c4710d6dd26b7ac7a6ec97d3d76a713
GMDancun <dantrevor18@gmail.com>
2023-03-21 21:55:55 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+
+	_putchar('\n');
+}
