4ab6dc05f0ce9e3e011698a97909ae4715826529
GMDancun <dantrevor18@gmail.com>
2023-03-28 23:58:35 +0000

    Tasks

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input character
+ * Return: print one character
+ */
+void puts2(char *str)
+{
+	int j = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		j++;
+	}
+	t = j - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
+
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input string
+ */
+void puts_half(char *str)
+{
+	int a, n, j;
+
+	j = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		j++;
+
+	n = (j / 2);
+
+	if ((j % 2) == 1)
+		n = ((j + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
+
