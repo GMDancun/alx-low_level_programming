22c699fa8dedad4f859321e253461a926f7f06b7
GMDancun <dantrevor18@gmail.com>
2023-04-28 23:00:10 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,19 +11,19 @@
 size_t print_list(const list_t *h)
 {
 	unsigned int j;
-	
+
 	j = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
+		}
 		j++;
+		h = h->next;
 	}
 	return (j);
 }
