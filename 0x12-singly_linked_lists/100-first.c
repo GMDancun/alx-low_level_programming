ee71e4997494c73e40299e240277f929ac9e3e48
GMDancun <dantrevor18@gmail.com>
2023-04-29 02:26:20 +0000

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -13,6 +13,6 @@ void print(void) __attribute__ ((constructor));
 void print(void)
 {
 	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back! \n");
+	printf("\nI bore my house upon my back!\n");
 }
 
