946070a6ea030dc3d25097def2631bc47c1ce972
GMDancun <dantrevor18@gmail.com>
2023-04-29 02:21:06 +0000

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 
-void print_beforemain(void) __attribute__((constructor));
-/*
- * printbeforemain - prints a sentence before the main function
+void print(void) __attribute__ ((constructor));
+
+/**
+ * print - prints before the main function
  *
+ * Description - function that prints a sentence before the main function
  *
  */
 
-void print_beforemain(void)
+void print(void)
 {
 	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back! \n");
