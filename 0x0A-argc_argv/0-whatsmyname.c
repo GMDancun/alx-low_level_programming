8cc9909243104865e4503e01928e25a33c9175ba
GMDancun <dantrevor18@gmail.com>
2023-05-02 08:35:07 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name
+ * @argv: is the array name
+ * @argc: argument
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
