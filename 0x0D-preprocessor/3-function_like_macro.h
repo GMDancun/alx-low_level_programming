77e23136489808da816cd8f0490edc9b5bf58fff
GMDancun <dantrevor18@gmail.com>
2023-05-03 19:15:01 +0000

    preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/*
+ * Description: Header file that defines a function-like macro ABS(x)
+ * that computes the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
