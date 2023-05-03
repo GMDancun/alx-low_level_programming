9c3a30f358ae7fe1531ad46b7f329f5465a4c441
GMDancun <dantrevor18@gmail.com>
2023-05-03 20:08:39 +0000

    hot fix

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * sum_them_all - a function that returns the sum of all its parameters.
+ * @n: the variadic inputs
+ * Return: 0, if n == 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list vlst;
+	unsigned int u, s = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(vlst, n);
+
+	for (u = 0; u < n; u++)
+	{
+		sum += va_arg(vlst, const unsigned int);
+	}
+
+	va_end(vlst);
+
+	return (s);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - Entry Point
+ * c = char, i = int, f = float, s = char * (if null print (nil))
+ * @format: list of arg types
+ * Return: 0
+ */
+void print_all(const char * const format, ...)
+{
+	va_list vlst;
+	int q = 0, u = 0;
+	char *p = ", ";
+	char *t;
+
+	va_start(vlst, format);
+
+	while (format && format[u])
+		u++;
+
+	while (format && format[q])
+	{
+		if (q  == (u - 1))
+		{
+			p = "";
+		}
+		switch (format[q])
+		{
+		case 'c':
+			printf("%c%s", va_arg(vlst, int), p);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(vlst, int), p);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(vlst, double), p);
+			break;
+		case 's':
+			t = va_arg(vlst, char *);
+			if (t == NULL)
+				t = "(nil)";
+			printf("%s%s", t, p);
+			break;
+		}
+		q++;
+	}
+	printf("\n");
+	va_end(vlst);
+}
