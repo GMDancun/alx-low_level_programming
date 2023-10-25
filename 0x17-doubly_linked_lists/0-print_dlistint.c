a7cdc5027b1a394e3b8351ee9abd0800dc8fa73f
GMDancun <dantrevor18@gmail.com>
2023-10-25 19:35:18 +0000

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,20 +3,20 @@
 /**
  * print_dlistint - a program that prints * the elements of...
  * a double linked list...
- * @h
- * 
+ * @h: - description
+ *
  * Return: The number of the nodes of the double-linked list...
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int breadth = 0;
+	int breadth = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        ++breadth;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		++breadth;
+		h = h->next;
+	}
 
-    return (breadth);
+	return (breadth);
 }
